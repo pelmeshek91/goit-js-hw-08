547ed50c0953c9ce6cbb31a61a624c3718b52653
Lyuba Pushkina <lyuba.pushkina@gmail.com>
2022-08-31 18:21:08 +0100

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Change code below this line
+const list = document.querySelector('.gallery');
+const newImg = galleryItems
+  .map(
+    image =>
+      `<a class="gallery__item" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+        data-source="${image.original}"                      
+      alt="${image.description}"
+    />
+  </a>`
+  )
+  .join('');
+
+list.insertAdjacentHTML('beforeend', newImg);
+const lightbox = new SimpleLightbox('.gallery .gallery__item', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+document.addEventListener('click', e => {
+  e.preventDefault();
+  console.dir(e);
+  console.dir(document);
+});
+console.log(lightbox);
 console.log(galleryItems);
+// Change code below this line
