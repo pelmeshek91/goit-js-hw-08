699548ff99d3c5b266bab529d9f5fe6a1dc654bd
Lyuba Pushkina <lyuba.pushkina@gmail.com>
2022-09-01 09:12:46 +0100

    first steps

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on(STORAGE_KEY, function (data) {});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
