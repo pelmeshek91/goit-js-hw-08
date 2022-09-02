6bf800384a1cc6004535693bc43138184c2d795a
Lyuba Pushkina <lyuba.pushkina@gmail.com>
2022-09-02 12:26:16 +0100

    mission completed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,20 +8,26 @@ form.addEventListener('input', throttle(onGetData, 500));
 form.addEventListener('submit', sendForm);
 
 function onGetData(event) {
-  formValue[event.target.name] = event.target.value;
-
+  formValue[event.target.name] = event.target.value; /* запитати у Руслана */
+  console.dir(formValue);
   const { email, message } = formValue;
-
   localStorage.setItem(SAVED_KEY, JSON.stringify({ email, message }));
-  const savedMessage = localStorage.getItem(SAVED_KEY);
-  console.log(savedMessage);
 }
+
+function saveMessage() {
+  const obj = JSON.parse(localStorage.getItem(SAVED_KEY));
+  const textarea = document.querySelector('textarea');
+  const input = document.querySelector('input');
+  if (localStorage.getItem(SAVED_KEY)) {
+    textarea.value = obj.message;
+    input.value = obj.email;
+  }
+}
+saveMessage();
 
 function sendForm(event) {
   event.preventDefault();
-  /*  console.log(event.currentTarget);     
-  ---------target/currentTarget-------
-  console.log(event.target); */
-  event.target.reset();
+  console.log(formValue);
+  event.target.reset(); /* запитати у Руслана про currentTarget/target */
   localStorage.removeItem(SAVED_KEY);
 }
