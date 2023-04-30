2617ce7cf77a9bf94a2c45e44124376554dda024
Lyuba Pushkina <lyuba.pushkina@gmail.com>
2023-04-30 17:39:54 +0100

    complete task 3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,10 +1,9 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Описаний в документації
+
 import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
+
 import 'simplelightbox/dist/simple-lightbox.min.css';
-// Change code below this line
+
 import galleryItemsTmp from '../template/gallery-items.hbs';
 
 const galleryList = document.querySelector('.gallery');
