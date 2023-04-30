2040a8bd92dd304adc23d856037b5dea5c3314fb
Lyuba Pushkina <lyuba.pushkina@gmail.com>
2023-04-30 11:52:41 +0100

    comlete task 1 with hbs

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,18 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+import galleryItemsTmp from '../template/gallery-items.hbs';
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+const markup = galleryItemsTmp(galleryItems);
+
+galleryList.insertAdjacentHTML('beforeend', markup);
+
+const lightbox = new SimpleLightbox('.gallery .gallery__link', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
